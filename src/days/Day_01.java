97fbc9b80323c0c5ee1cfbd131d1a445330a14e0
Francesco <114349231+Frvnce@users.noreply.github.com>
2024-12-02 20:28:18 +0100

    Day 01

---
+++ src/days/Day_01.java
@@ -0,0 +1,22 @@
+package days;
+
+public class Day_01 {
+   /*
+    Per esempio:
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+
+    Abbina i numeri e misura la loro distanza.
+    Abbina il numero più piccolo nella lista di sinistra con il numero più piccolo nella lista di destra
+    poi il secondo numero più piccolo a sinistra con il secondo numero più piccolo a destra , e così via.
+
+    All'interno di ogni coppia, calcola quanto sono distanti i due numeri;
+    dovrai sommare tutte quelle distanze .
+    Ad esempio, se abbini a 3dall'elenco di sinistra con a 7dall'elenco di destra
+    la distanza è 4; se abbini a 9con a 3, la distanza è 6.
+    */
+}
